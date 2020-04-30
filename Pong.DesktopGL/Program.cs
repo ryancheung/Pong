3bd8a805724e4dc59d8430e2899b48a433633561
ryancheung <ryancheung.go@gmail.com>
2020-04-30 21:08:26 +0800

    Pong

---
+++ Pong.DesktopGL/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Pong.DesktopGL
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
